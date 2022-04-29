be403510cb57c3c81a4f7e47bd7cb00778f8ecc8
mgoliath <mgoliath@student.21-school.ru>
2022-04-29 02:25:59 -0700

    cub3D

---
+++ cmake-build-debug/CMakeFiles/cub3d.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/mtifany/CLionProjects/cub3d/src/ft_check.c" "CMakeFiles/cub3d.dir/src/ft_check.c.o" "gcc" "CMakeFiles/cub3d.dir/src/ft_check.c.o.d"
+  "/Users/mtifany/CLionProjects/cub3d/src/ft_create_tiles.c" "CMakeFiles/cub3d.dir/src/ft_create_tiles.c.o" "gcc" "CMakeFiles/cub3d.dir/src/ft_create_tiles.c.o.d"
+  "/Users/mtifany/CLionProjects/cub3d/src/ft_details_display.c" "CMakeFiles/cub3d.dir/src/ft_details_display.c.o" "gcc" "CMakeFiles/cub3d.dir/src/ft_details_display.c.o.d"
+  "/Users/mtifany/CLionProjects/cub3d/src/ft_display.c" "CMakeFiles/cub3d.dir/src/ft_display.c.o" "gcc" "CMakeFiles/cub3d.dir/src/ft_display.c.o.d"
+  "/Users/mtifany/CLionProjects/cub3d/src/ft_free.c" "CMakeFiles/cub3d.dir/src/ft_free.c.o" "gcc" "CMakeFiles/cub3d.dir/src/ft_free.c.o.d"
+  "/Users/mtifany/CLionProjects/cub3d/src/ft_free1.c" "CMakeFiles/cub3d.dir/src/ft_free1.c.o" "gcc" "CMakeFiles/cub3d.dir/src/ft_free1.c.o.d"
+  "/Users/mtifany/CLionProjects/cub3d/src/ft_move.c" "CMakeFiles/cub3d.dir/src/ft_move.c.o" "gcc" "CMakeFiles/cub3d.dir/src/ft_move.c.o.d"
+  "/Users/mtifany/CLionProjects/cub3d/src/ft_panel.c" "CMakeFiles/cub3d.dir/src/ft_panel.c.o" "gcc" "CMakeFiles/cub3d.dir/src/ft_panel.c.o.d"
+  "/Users/mtifany/CLionProjects/cub3d/src/ft_ray_casting.c" "CMakeFiles/cub3d.dir/src/ft_ray_casting.c.o" "gcc" "CMakeFiles/cub3d.dir/src/ft_ray_casting.c.o.d"
+  "/Users/mtifany/CLionProjects/cub3d/src/ft_ray_casting_hor_vert.c" "CMakeFiles/cub3d.dir/src/ft_ray_casting_hor_vert.c.o" "gcc" "CMakeFiles/cub3d.dir/src/ft_ray_casting_hor_vert.c.o.d"
+  "/Users/mtifany/CLionProjects/cub3d/src/ft_read_map.c" "CMakeFiles/cub3d.dir/src/ft_read_map.c.o" "gcc" "CMakeFiles/cub3d.dir/src/ft_read_map.c.o.d"
+  "/Users/mtifany/CLionProjects/cub3d/src/ft_textures.c" "CMakeFiles/cub3d.dir/src/ft_textures.c.o" "gcc" "CMakeFiles/cub3d.dir/src/ft_textures.c.o.d"
+  "/Users/mtifany/CLionProjects/cub3d/src/get_next_line.c" "CMakeFiles/cub3d.dir/src/get_next_line.c.o" "gcc" "CMakeFiles/cub3d.dir/src/get_next_line.c.o.d"
+  "/Users/mtifany/CLionProjects/cub3d/src/get_next_line_utils.c" "CMakeFiles/cub3d.dir/src/get_next_line_utils.c.o" "gcc" "CMakeFiles/cub3d.dir/src/get_next_line_utils.c.o.d"
+  "/Users/mtifany/CLionProjects/cub3d/src/main.c" "CMakeFiles/cub3d.dir/src/main.c.o" "gcc" "CMakeFiles/cub3d.dir/src/main.c.o.d"
+  "/Users/mtifany/CLionProjects/cub3d/src/parse.c" "CMakeFiles/cub3d.dir/src/parse.c.o" "gcc" "CMakeFiles/cub3d.dir/src/parse.c.o.d"
+  "/Users/mtifany/CLionProjects/cub3d/src/parse_args_utils.c" "CMakeFiles/cub3d.dir/src/parse_args_utils.c.o" "gcc" "CMakeFiles/cub3d.dir/src/parse_args_utils.c.o.d"
+  "/Users/mtifany/CLionProjects/cub3d/src/parse_args_utils2.c" "CMakeFiles/cub3d.dir/src/parse_args_utils2.c.o" "gcc" "CMakeFiles/cub3d.dir/src/parse_args_utils2.c.o.d"
+  "/Users/mtifany/CLionProjects/cub3d/src/parse_map.c" "CMakeFiles/cub3d.dir/src/parse_map.c.o" "gcc" "CMakeFiles/cub3d.dir/src/parse_map.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
